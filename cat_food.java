e14dcda66eaffc0dd686499ed62c5f3b9ee55cb8
nikita242114 <5358711@mail.ru>
2023-10-01 12:46:22 +0500

    work1

--- cat_food.java
+++ cat_food.java
@@ -47,7 +47,7 @@ class Cat {
 public class Main {
     public static
     void main(String[] args) {
-        Cat[] cats = {new Cat("Vasilij", 50), new Cat("Bombilo", 50), new Cat("Snezhok", 25), new Cat("Kuzma", 5), new Cat("Kote", 1)};
+        Cat[] cats = {new Cat("Vasilij", 50), new Cat("Barsik", 50), new Cat("Snezhok", 25), new Cat("Kuzma", 5), new Cat("Kote", 1)};
 Plate plate = new Plate( 80);
  
         for (Cat cat : cats) {
